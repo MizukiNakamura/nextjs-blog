698118329476e691848693cafcbd0727e2b17a96
MizukiNakamura <himuusagi1127@gmail.com>
2022-03-19 20:08:26 +0900

    ブログアイテムのホバー時デザイン変更 ブログアイテムのリンク機能追加

--- components/BlogList.jsx
+++ components/BlogList.jsx
@@ -1,6 +1,6 @@
 import nl2br from "react-nl2br";
-// import '../styles/BlogList.module.css'
 import styles from '../styles/BlogList.module.css'
+import Link from "next/link";
 
 export default function BlogList({ posts }) {
   return (
@@ -8,17 +8,21 @@ export default function BlogList({ posts }) {
       {posts.contents.map((post, index) => {
         let content = post.content
         const len = content.length
-        content = (len >= 35) ? content.substr(0, 35) + '...' : content
+        content = (len >= 35) ? content.substring(0, 35) + '...' : content
 
         return (
           <li key={index} className={styles.blogItem}>
-            <h2 className={styles.blogItem__heading}>{post.title}</h2>
-            <p className={styles.blogItem__content}>
-              {nl2br(content)}
-            </p>
-            <figure className={styles.blogItem__imgWrapper}>
-              <img className={styles.blogItem__img} src={post.image.url} />
-            </figure>
+            <Link href={`/blog/${post.title}`}>
+              <a className={styles.blogItem__link}>
+                <h2 className={styles.blogItem__heading}>{post.title}</h2>
+                <p className={styles.blogItem__content}>
+                  {nl2br(content)}
+                </p>
+                <figure className={styles.blogItem__imgWrapper}>
+                  <img className={styles.blogItem__img} src={post.image.url} />
+                </figure>
+              </a>
+            </Link>
           </li>
         );
       })}
