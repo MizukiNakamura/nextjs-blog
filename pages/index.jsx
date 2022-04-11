5cd9b52b7670b5c8665c9568f3592f5bc7fe7497
MizukiNakamura <himuusagi1127@gmail.com>
2022-04-11 19:45:44 +0900

    lib/postsの読み込みエラー解消

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,12 @@
 import Layout from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-// import fetch from 'node-fetch'
-
-export async function getServerSideProps() {
-	const allPostsData = getSortedPostsData()
-	return {
-		props: { allPostsData }
-	}
-}
 
 export default function Home({ allPostsData }) {
 	return (
 		<Layout home>
 			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
 				<h2 className={utilStyles.headingLg}>Blog</h2>
-				<ul className={utilStyles.list}>
+				{/* <ul className={utilStyles.list}>
 					{allPostsData.map(({ id, date, title }) => (
 						<li className={utilStyles.listItem} key={id}>
 							title: "{title}"
@@ -25,7 +16,7 @@ export default function Home({ allPostsData }) {
 							date: {date}
 						</li>
 					))}
-				</ul>
+				</ul> */}
 			</section>
 		</Layout>
 	)
