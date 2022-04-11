d293fc61b6cfca2dd4cf8a56461ca4fedf7272bd
MizukiNakamura <himuusagi1127@gmail.com>
2022-04-11 19:47:07 +0900

    topページタイトル変更

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,11 @@
 import Layout from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home({ allPostsData }) {
+export default function Home() {
 	return (
 		<Layout home>
 			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-				<h2 className={utilStyles.headingLg}>Blog</h2>
-				{/* <ul className={utilStyles.list}>
-					{allPostsData.map(({ id, date, title }) => (
-						<li className={utilStyles.listItem} key={id}>
-							title: "{title}"
-							<br />
-							id: {id}
-							<br />
-							date: {date}
-						</li>
-					))}
-				</ul> */}
+				<h2 className={utilStyles.headingLg}>Next.js Blog</h2>
 			</section>
 		</Layout>
 	)
